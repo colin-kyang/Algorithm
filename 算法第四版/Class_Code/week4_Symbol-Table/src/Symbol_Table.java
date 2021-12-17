87c3c1fa7875e032d873cfbefe6e6eefe268c496
kyang <kyang@stu.ecnu.edu.cn>
2021-12-17 20:18:09 +0800

    2021/12/17

--- 算法第四版/Class_Code/week4_Symbol-Table/src/Symbol_Table.java
+++ 算法第四版/Class_Code/week4_Symbol-Table/src/Symbol_Table.java
@@ -1,8 +1,9 @@
 /**
- * 符号表：类map
+ * 符号表：类似于 map<key,value>
  * 基于二叉搜素树实现
  */
 public class Symbol_Table<Key extends Comparable<Key>,Value> {
+
     class Node{
         Key key;
         Value value;
@@ -11,7 +12,7 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
         int size;
         //init without param
         public Node(){
-           size=0;
+            size=0;
         }
         //init with param
         public Node(Key key,Value value)
@@ -20,7 +21,11 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
             this.value=value;
         }
     }
+
+    //根节点
     private Node root;
+
+
     /**
      * 初始化条件
      */
@@ -29,12 +34,14 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
     }
 
     /**
-     * 返回根节点
-     * @return
+     * 供对象调用 插值
+     * @param key
+     * @param value
      */
-    public Node getRoot()
+    public void put(Key key,Value value)
     {
-        return this.root;
+        this.root=put(this.root,key,value);
+        size(this.root);
     }
 
     /**
@@ -47,8 +54,7 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
         if(current==null)
         {
             //插入节点
-            current=new Node(key,value);
-            return current;
+           return new Node(key,value);
         }
         int flag=key.compareTo(current.key);
         if(flag==0)
@@ -59,7 +65,7 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
         else if(flag<0)
         {
             //若插入节点 key < current.value : 左子树搜索
-            current.left=put(current.left,key,value);
+           current.left=put(current.left,key,value);
         }
         else
         {
@@ -70,12 +76,13 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
     }
 
     /**
+     * 根据key 取值
      * @param key
      * @return
      */
     public Value get(Key key)
     {
-        Node current=root;
+        Node current=this.root;
         while(current!=null)
         {
             int flag=key.compareTo(current.key);
@@ -85,30 +92,152 @@ public class Symbol_Table<Key extends Comparable<Key>,Value> {
             }
             else if(flag<0)
             {
-                current=current.right;
+                //key 较小：左子树搜索
+                current=current.left;
             }
             else
             {
-                current=current.left;
+                //key 较大： 右子树搜索
+                current=current.right;
             }
         }
         return null;
     }
 
-//    public delete(Key key)
-//    {
-//
-//    }
-//
+    /**
+     * 节点size的获取（以cuurent 节点为根的子树，共存在多少节点）
+     * @param current
+     * @return
+     */
+    public int size(Node current)
+    {
+        if(current==null){return 0;}
+        return (current.size= 1+size(current.left)+size(current.right));
+    }
+
+    /**
+     * 获取key 值最大节点
+     * @return
+     */
+    public Node getMax(Node root)
+    {
+        Node current=root;
+        while(current.right!=null)
+        {
+            current=current.right;
+        }
+        return current;
+    }
+
+    /**
+     * 获取key 值最小节点
+     * @return
+     */
+    public Node getMin(Node root)
+    {
+        Node current=root;
+        while(current.left!=null)
+        {
+            current=current.left;
+        }
+        return current;
+    }
+
+
+    /**
+     * largest key <= a given key
+     * @param key
+     * @return
+     */
+    public Node Floor(Key key)
+    {
+      return Floor(this.root,key);
+    }
+
+
+    /**
+     *
+     * @param current
+     * @param key
+     * @return
+     */
+    public Node Floor(Node current,Key key)
+    {
+        if(current ==null)
+        {
+            return null;
+        }
+        int flag=key.compareTo(current.key);
+        if(flag==0)
+        {
+            return current;
+        }
+        else if(flag<0)
+        {
+            return Floor(current.left,key);
+        }
+        Node tmp=Floor(current.right,key);
+        if(tmp==null)
+        {//试探：可能存在回退操作
+            return current;
+        }
+        else
+        {
+            return tmp;
+        }
+    }
+
+    /**
+     * 从root 开始扫描，并开始删除点
+     * @param key
+     */
+    public void remove(Key key)
+    {
+        remove(this.root,key);
+    }
+
+    public Node remove(Node current,Key key)
+    {
+        if(current==null)
+        {
+            //不存在目标节点
+            return null;
+        }
+        int flag=key.compareTo(current.key);
+        if(flag==0)
+        {
+           if(current.right==null){ return current.left;}
+           if(current.left==null){return current.right;}
+           //左右子树都存在：将右子树，最小的哪个节点获取
+            Node rMin=getMin(current.right);
+            remove(current.right,rMin.key);
+            rMin.left=current.left;
+            rMin.right=current.right;
+            current=rMin;
+        }
+        else if(flag>0)
+        {
+            //右子树删除操作
+            current.right=remove(current.right,key);
+        }
+        else if(flag<0)
+        {
+            //左子树删除操作
+            current.left=remove(current.left,key);
+        }
+        size(current);
+        return current;
+    }
+
 
     public static void main(String[] args) {
         //基本构建
         Symbol_Table<Integer,Integer> st=new Symbol_Table<>();
-        st.put(st.getRoot(),1,9190);
-        st.put(st.getRoot(),2,9890);
-        st.put(st.getRoot(),3,9999);
-        System.out.println(st.get(1));
-        System.out.println(st.get(2));
+        st.put(3,9190);
+        st.put(2,9890);
+        st.put(1,9999);
+        st.put(4,10);
+        st.remove(1);
         System.out.println(st.get(3));
     }
 }
